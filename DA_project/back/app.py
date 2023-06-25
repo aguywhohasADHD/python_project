03bf245261fd07161e2a037ad880f0a96bcad786
aguywhohasADHD <119142963+aguywhohasADHD@users.noreply.github.com>
2023-06-25 12:11:13 +0900

    Add files via upload
    2023년 6월 25일 일요일

---
+++ DA_project/back/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import numpy as np
+#from flask_cors import CORS
+import json
+import pandas as pd
+app=Flask(__name__)
+#CORS(app)
+@app.route('/')
+def main():
+    labels=["January","February","March","April","May","June"]
+    l={
+        'labels':labels,
+        'datasets':[
+            {
+                'label':"My First dataset",
+                'backgroundColor':"rgb(255, 99, 132)",
+                'borderColor':"rgb(255, 99, 132)",
+                'data':[50, 5, 2, 7, 30, 20, 45],
+            },
+        ],
+    }
+    res=json.dumps(l)
+    return res
+@app.route('/test')
+if __name__=='__main__':
+    app.run()
