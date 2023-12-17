ab2588c20f3f66ba1d31e494074575ded7d2a8d9
aguywhohasADHD <jaehyeongwag56@gmail.com>
2023-12-17 12:26:44 +0900

    :memo:update

---
+++ useable_data_learning/uniqueAndCount01.py
@@ -0,0 +1,15 @@
+from pandas import Series
+print('\n#유일 값, 값 세기, 멤버십')
+mylist=['라일락','ㅋ코스모스','코스모스','백일홍','코스모스','코스모스','들장미','들장미','라일락','라일락']
+myseries=Series(mylist)
+print(myseries)
+myunique=myseries.unique()
+print(myunique)
+print(myseries.value_counts(normalize=True))
+print(myseries.value_counts())
+#'돌장미','라일락'이 들어 있는 색인만 추출하겠습니다.
+#데이터베이스의 in 절(clause)과 유사한 개념으로 봐도 무방합니다.
+mask=myseries.isin(['돌장미','라일락'])
+print(mask)
+print('-'*30)
+print(myseries[mask])
